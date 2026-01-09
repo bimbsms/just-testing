aa16fe7b5ae29a0cf54850ce7ae111fad1f92a51
bimbsms <79249028+bimbsms@users.noreply.github.com>
2026-01-09 10:29:53 +0800

    Add print statement for debugging

--- main.py
+++ main.py
@@ -7,7 +7,7 @@ def demo_function(request):
     HTTP entry point for Cloud Functions Gen 2
     """
     result = do_work()
-
+    print("huhu")
     return jsonify({
         "message": "Cloud Functions Gen 2 demo",
         "result": result,
