4b29f7aa3870359a808605054f652298c75764d5
bimbsms <79249028+bimbsms@users.noreply.github.com>
2026-01-09 10:04:16 +0800

    Add demo_function for Cloud Functions Gen 2

---
+++ main.py
@@ -0,0 +1,15 @@
+from flask import jsonify
+from utils import do_work
+from config import config_summary
+
+def demo_function(request):
+    """
+    HTTP entry point for Cloud Functions Gen 2
+    """
+    result = do_work()
+
+    return jsonify({
+        "message": "Cloud Functions Gen 2 demo",
+        "result": result,
+        "config": config_summary(),
+    })
